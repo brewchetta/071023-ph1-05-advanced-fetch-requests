f4f3e8d1f893b1bf395c2c6b13b2795d03671304
brewchetta <chett.tiller@gmail.com>
2023-07-17 12:02:29 -0400

    aliens amogus

--- src/index.js
+++ src/index.js
@@ -17,8 +17,30 @@ newSightingForm.addEventListener('submit', event => {
   event.preventDefault()
   const sightingInput = event.target['new-sighting-input']
   const sightingInputValue = sightingInput.value
-  addSightingToList(sightingInputValue)
+
+  const POST_OPTIONS = {
+    method: "POST",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ content: sightingInputValue })
+  }
+
+  fetch("http://localhost:3000/sightings", POST_OPTIONS)
+    .then( response => response.json() )
+    .then( data => {
+      console.log( "new sighting:", data )
+      addSightingToList( data.content )
+    })
+    .catch( error => alert( error ) )
+    .finally( () => alert("I am the finally!") )
+
+    // try and do something / catch if there's an error and do something else
+
+  event.target.reset()
 })
+
 
 // --- FETCH ON LOAD --- //
 
